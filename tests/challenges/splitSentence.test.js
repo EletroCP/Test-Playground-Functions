596c4ea2603c2b71336c083c0f2ef4a250794ab9
eletrocp <brernolopes@hotmail.com>
2022-10-07 16:11:41 -0300

    :tube_test: test: Teste unitario da função "splitSentence"

---
+++ tests/challenges/splitSentence.test.js
@@ -0,0 +1,22 @@
+const { splitSentence } = require('../../src/challenges');
+
+describe(`Verifica se o retorno da função "splitSentence" é igual ao array 
+  contendo todas as palavras da frase`, () => {
+
+    const phraseToSplit = 'frase de teste para verificar o output da função';
+    const arrayLength = 9;
+
+    it('Verifica se um array é retornado ao chamar a função', () => {
+      expect(splitSentence(phraseToSplit)).toBeInstanceOf(Array);
+    });
+
+    it('Verfirica se possui a quantidade correta de palavras', () => {
+      expect(splitSentence(phraseToSplit)).toHaveLength(arrayLength);
+    });
+    it('Verfirica se a primeira palavra do array é "frase"', () => {
+      expect(splitSentence(phraseToSplit)[0]).toBe("frase");
+    });
+    it('Verfirica se a ultima palavra do array é "função"', () => {
+      expect(splitSentence(phraseToSplit)[arrayLength - 1]).toBe("função");
+    });
+  });
